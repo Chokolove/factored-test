982bbaf284443e488fd9e6823a05f446852a89f9
Carlos LL <carlosll2493@gmail.com>
2025-06-12 02:02:21 -0500

    Add web pagination

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Container from "@/components/Container";
 import IntroText from "@/components/IntroText";
 import Pagination from "@/components/Pagination";
@@ -32,25 +33,36 @@ const HEADERS = [
   "Birth Year",
   "Gender",
 ];
+
 export default function Characters() {
   const [page, setPage] = useState(1);
   const [name, setName] = useState("");
   const [debounceName] = useDebounce(name, 500);
+
   const { data, isLoading, isFetching, isError } = useGetCharactersQuery({
     page,
     name: debounceName,
   });
+
   const people = data?.results ?? data?.result ?? [];
-  const showPagination = !name && data?.total_pages && people?.length > 0;
+  const isSearching = !!debounceName;
+  const perPage = 4;
+
+  const visiblePeople = isSearching
+    ? people.slice((page - 1) * perPage, page * perPage)
+    : people;
+
+  const totalPages = isSearching
+    ? Math.ceil(people.length / perPage)
+    : data?.total_pages || 1;
 
   useEffect(() => {
     setPage(1);
-  }, [name]);
+  }, [debounceName]);
 
   return (
     <Container>
       <IntroText title="Meet the Legends Behind the Saga" text={introText} />
-
       <RequestFeedback isError={isError} isLoading={isLoading} />
 
       {!isLoading && data && (
@@ -66,48 +78,50 @@ export default function Characters() {
 
           <Table
             tableHeaders={HEADERS}
-            data={people}
-            renderRow={(film, index) => {
-              return isFetching ? (
-                [...Array(1)].map((_, index) => (
+            data={visiblePeople}
+            renderRow={(character, index) => {
+              if (isFetching || isLoading) {
+                return (
                   <SkeletonRow
-                    key={index}
+                    key={`skeleton-${index}`}
                     index={index}
                     columns={HEADERS.length}
                   />
-                ))
-              ) : (
+                );
+              }
+
+              const person = character.properties;
+              return (
                 <tr
-                  key={film._id}
+                  key={character._id}
                   className={`transition duration-200 ${
                     index % 2 === 0 ? "bg-[#2c2c2c]" : "bg-[#1f1f1f]"
                   } hover:bg-light-blue-soft`}
                 >
-                  <td className="px-6 py-4 font-medium">
-                    {film.properties.name}
-                  </td>
-                  <td className="px-6 py-4">{film.properties.height} cm</td>
-                  <td className="px-6 py-4">{film.properties.mass} kg</td>
-                  <td className="px-6 py-4">{film.properties.hair_color}</td>
-                  <td className="px-6 py-4">{film.properties.skin_color}</td>
-                  <td className="px-6 py-4">{film.properties.eye_color}</td>
-                  <td className="px-6 py-4">{film.properties.birth_year}</td>
-                  <td className="px-6 py-4">{film.properties.gender}</td>
+                  <td className="px-6 py-4 font-medium">{person.name}</td>
+                  <td className="px-6 py-4">{person.height} cm</td>
+                  <td className="px-6 py-4">{person.mass} kg</td>
+                  <td className="px-6 py-4">{person.hair_color}</td>
+                  <td className="px-6 py-4">{person.skin_color}</td>
+                  <td className="px-6 py-4">{person.eye_color}</td>
+                  <td className="px-6 py-4">{person.birth_year}</td>
+                  <td className="px-6 py-4">{person.gender}</td>
                 </tr>
               );
             }}
           />
-          {showPagination && (
+
+          {people.length > 0 && (
             <Pagination
               previesPage={() =>
                 setPage((prev) => (prev - 1 === 0 ? 1 : prev - 1))
               }
               nextPage={() =>
-                setPage((prev) => (prev + 1 > data.total_pages ? 1 : prev + 1))
+                setPage((prev) => (prev + 1 > totalPages ? 1 : prev + 1))
               }
               page={page}
               setPage={setPage}
-              totalPages={data.total_pages}
+              totalPages={totalPages}
               isFetching={isFetching}
             />
           )}
