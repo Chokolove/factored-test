f7aa336c6589dc05bb56541db29058d45af6868d
Carlos LL <carlosll2493@gmail.com>
2025-06-11 19:27:32 -0500

    Add new color consul-green

--- src/components/FeaturedFilmCard.tsx
+++ src/components/FeaturedFilmCard.tsx
@@ -3,7 +3,7 @@ import AnimatedTitle from "./AnimatedTitle";
 
 export default function FeaturedFilmCard() {
   return (
-    <div className="group  border border-white flex flex-col md:flex-row rounded-lg bg-imperial-gray shadow-lg hover:shadow-white text-white transition-shadow duration-1000">
+    <div className="group  border border-white flex flex-col md:flex-row rounded-lg bg-imperial-gray shadow-lg hover:shadow-consul-green text-white transition-shadow duration-1000">
       <div className="relative w-full md:w-full min-h-[300px]">
         <Image
           src="https://media.brisa-bbb.com/sw-fanPage/movies.jpg"
@@ -17,7 +17,10 @@ export default function FeaturedFilmCard() {
       {/* Text Content */}
       <div className=" flex flex-col justify-between py-4 pr-4">
         <div className="p-6 flex flex-col justify-center gap-4 flex-1">
-          <AnimatedTitle title={"The Saga of the Stars"} />
+          <AnimatedTitle
+            title={"The Saga of the Stars"}
+            hoverClass="group-hover:text-consul-green"
+          />
           <p className="text-sm text-white/80">
             From the twin suns of Tatooine to the icy fields of Hoth, the Star
             Wars films have told tales of hope, darkness, and redemption. Across
